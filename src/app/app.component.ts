d000334f75ef7493c83fd2e17af2a0894074e6ac
usagi-f <usagi.f@gmail.com>
2017-07-27 02:05:02 +0900

    update: getlist reverse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import "rxjs/add/operator/map";
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,17 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 })
 export class AppComponent {
   title: string;
+  db: AngularFireDatabase;
   getlist: FirebaseListObservable<any[]>;
   length: number;
   constructor(db: AngularFireDatabase) {
     this.title = 'togoshi-web';
-    this.getlist = db.list('/getlist');
+    this.db = db;
+    this.getlist = this.db.list('/getlist', {
+      query: {
+        orderByChild: 'time'
+      }
+    }).map((array) => array.reverse()) as FirebaseListObservable<any[]>;
     this.getlist.subscribe(snapshot => {
         this.length = snapshot.length;
       });
