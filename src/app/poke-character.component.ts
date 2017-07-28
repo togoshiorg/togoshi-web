c4d42291fb0d2811233ac5fa159cf529f077d760
usagi.f@gmail.com <usagi.f@gmail.com>
2017-07-28 18:22:55 +0900

    update: chara component display data

--- src/app/poke-character.component.ts
+++ src/app/poke-character.component.ts
@@ -3,20 +3,34 @@ import { translateData } from './data/pokemon';
 
 @Component({
   selector: 'poke-character',
-  template: `<img src={{url}}>`
+  template: `
+    <div class="chara">
+      <div class="img">
+        <img src={{url}}>
+      </div>
+      <div class="status">
+        <h1>{{nameJa}}</h1>
+        <p><span>図鑑No:</span>{{item.id}}</p>
+        <p><span>CP:</span>{{item.cp}}</p>
+        <p><span>時間:</span>{{item.time}}</p>
+        <p><span>親:</span>{{item.user}}</p>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./poke-character.component.css']
 })
 export default class SpriteComponent {
-  @Input() id: number;
-  @Input() isShiny: boolean;
-  name: string;
+  @Input() item: any;
+  nameEn: string;
+  nameJa: string;
   url: string;
   translateData: Array<any>;
   constructor() {
     this.translateData = translateData;
   }
   ngOnInit() {
-    const number = this.id - 1;
-    this.name = this.translateData[number].en.toLowerCase();
-    this.url = `http://www.pokestadium.com/sprites/xy/${this.isShiny?'shiny/':''}${this.name}.gif`;
+    this.nameEn = this.translateData[this.item.id - 1].en.toLowerCase();
+    this.nameJa = this.translateData[this.item.id - 1].ja.toLowerCase();
+    this.url = `http://www.pokestadium.com/sprites/xy/${this.item.isShiny?'shiny/':''}${this.nameEn}.gif`;
   }
 }
