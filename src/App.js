46ccb13b96f5ea341c5ca28ca56cf8d9e0b96d95
usagi-f <usagi.f@gmail.com>
2017-09-07 01:41:45 +0900

    update: display firebase db

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import fbobj from './firebase/';
 import './App.css';
 
+const getlist = fbobj.db.ref('getlist');
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      getlistArray: [],
+    };
+  }
+  componentDidMount() {
+    getlist.once('value').then((snapshot) => {
+      const firebaseSnapshot = snapshot.val();
+      const snapshotArray = Object.keys(firebaseSnapshot).map((value) => {
+        return firebaseSnapshot[value];
+      });
+      this.setState({
+        getlistArray: snapshotArray,
+      });
+    });
+  }
+  getlist() {
+    return this.state.getlistArray.map((value, index) => {
+      return (
+        <li key={`${index}-${value.id}`}>
+          <div className="img">
+            画像
+          </div>
+          <div className="status">
+            <h2>名前:***</h2>
+            <p><span>図鑑No:</span>{value.id}</p>
+            <p><span>CP:</span>{value.cp}</p>
+            <p><span>時間:</span>{value.time}</p>
+            <p><span>親:</span>{value.user}</p>
+          </div> 
+        </li>
+      );
+    })
+  }
+  getlistArea() {
+    if (this.state.getlistArray.length === 0) {
+      return <p>読み込み中......</p>
+    } else {
+      return <ul>{this.getlist()}</ul>;
+    }
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{this.state.getlistArray.length}匹</h1>
+        {this.getlistArea()}
       </div>
     );
   }
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyDHvmfrV1H1AVR4eWfyRFE87VJ0PS3DFIw',
+    authDomain: 'getpokemon-99eed.firebaseapp.com',
+    databaseURL: 'https://getpokemon-99eed.firebaseio.com/',
+    projectId: 'getpokemon-99eed',
+    storageBucket: 'getpokemon-99eed.appspot.com',
+};
+
+export default firebaseConfig;
