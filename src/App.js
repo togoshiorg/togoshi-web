f6af199f5c5bd0699f8b2766f507965798472d3e
usagi-f <usagi.f@gmail.com>
2017-09-07 02:21:21 +0900

    add: semantic-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import fbobj from './firebase/';
+import { Container, Header, Icon, Card, Image } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 const getlist = fbobj.db.ref('getlist');
@@ -12,47 +14,54 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    getlist.once('value').then((snapshot) => {
-      const firebaseSnapshot = snapshot.val();
-      const snapshotArray = Object.keys(firebaseSnapshot).map((value) => {
-        return firebaseSnapshot[value];
-      });
-      this.setState({
-        getlistArray: snapshotArray,
-      });
+    getlist.once('value').then((snapshot) => this.updateList(snapshot));
+  }
+  updateList(snapshot) {
+    const firebaseSnapshot = snapshot.val();
+    const snapshotArray = Object.keys(firebaseSnapshot).map((value) => {
+      return firebaseSnapshot[value];
+    });
+    this.setState({
+      getlistArray: snapshotArray.reverse(),
     });
   }
   getlist() {
     return this.state.getlistArray.map((value, index) => {
       return (
-        <li key={`${index}-${value.id}`}>
-          <div className="img">
-            画像
-          </div>
-          <div className="status">
-            <h2>名前:***</h2>
-            <p><span>図鑑No:</span>{value.id}</p>
-            <p><span>CP:</span>{value.cp}</p>
-            <p><span>時間:</span>{value.time}</p>
-            <p><span>親:</span>{value.user}</p>
-          </div> 
-        </li>
+        <Card key={`${index}-${value.id}`}>
+          <Card.Content>
+            <Image floated='right' size='mini' src='http://www.pokestadium.com/sprites/xy/ditto.gif' />
+            <Card.Header>メタモン</Card.Header>
+            <Card.Meta>No: {value.id}</Card.Meta>
+            <Card.Description><Icon name='time' color="grey" /> {value.time}</Card.Description>
+            <Card.Description><Icon name='user' color="grey" /> {value.user}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Card.Description>CP: {value.cp}</Card.Description>
+          </Card.Content>
+        </Card>
       );
     })
   }
   getlistArea() {
     if (this.state.getlistArray.length === 0) {
-      return <p>読み込み中......</p>
+      return <div style={{ textAlign: 'center' }}><Icon name='spinner' color="grey" size="large" loading /></div>;
     } else {
-      return <ul>{this.getlist()}</ul>;
+      return <Card.Group style={{ justifyContent: 'center' }}>{this.getlist()}</Card.Group>;
     }
   }
   render() {
     return (
-      <div className="App">
-        <h1>{this.state.getlistArray.length}匹</h1>
+      <Container style={{ margin: 20 }}>
+        <Header as='h2' icon textAlign='center'>
+          <Icon name='gamepad' color="grey" />
+          <Header.Content>togoshi-web</Header.Content>
+        </Header>
+        <Header as='h3' textAlign='center'>
+          Total: {this.state.getlistArray.length}
+        </Header>
         {this.getlistArea()}
-      </div>
+      </Container>
     );
   }
 }
